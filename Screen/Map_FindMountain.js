fcb9ebbf16c6d619a5bcf7f01744e314df8ca253
JENNSHIAUN <JENNSHIUAN@github.com>
2022-06-25 13:25:11 +0800

    update date

--- Screen/Map_FindMountain.js
+++ Screen/Map_FindMountain.js
@@ -1,3 +1,4 @@
+// update date: 2022/6/25
 import React, { useEffect, useMemo, useState, Component } from 'react';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import Ionicons from 'react-native-vector-icons/Ionicons';
